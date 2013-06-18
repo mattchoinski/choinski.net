8974d537dad53507accc81e55fb5d585bbd5c969
mattchoinski <matt@choinski.net>
2013-06-18 15:10:18 -0400

    Add a link in a comment.

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $urlPath =
 			<section>
 				&nbsp;
 <!--
-That’s a very limited life. Life can be much broader, once you discover one simple fact, and that is that everything around you that you call life was made up by people that were no smarter than you. And you can change it, you can influence it, you can build your own things that other people can use. Once you learn that, you’ll never be the same again. - Steve Jobs
+That’s a very limited life. Life can be much broader, once you discover one simple fact, and that is that everything around you that you call life was made up by people that were no smarter than you. And you can change it, you can influence it, you can build your own things that other people can use. Once you learn that, you’ll never be the same again. - Steve Jobs (http://www.youtube.com/watch?feature=player_embedded&v=kYfNvmF0Bqw)
 -->
 			</section>
 		</div>
