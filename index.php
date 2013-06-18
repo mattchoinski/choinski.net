bb5db9bf25ebf333cea602d6606f626374235773
mattchoinski <matt@choinski.net>
2013-06-18 15:08:34 -0400

    Update the IE stylings, but there are still issues with rendering in IE browsers.

--- index.php
+++ index.php
@@ -62,6 +62,9 @@ $urlPath =
 			</section>
 			<section>
 				&nbsp;
+<!--
+That’s a very limited life. Life can be much broader, once you discover one simple fact, and that is that everything around you that you call life was made up by people that were no smarter than you. And you can change it, you can influence it, you can build your own things that other people can use. Once you learn that, you’ll never be the same again. - Steve Jobs
+-->
 			</section>
 		</div>
 		<footer class="columns two">
