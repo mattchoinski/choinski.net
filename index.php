3a113e457d7a4eb34bf15d5c90ddd79c59aaa5e0
mattchoinski <matt@choinski.net>
2013-12-17 15:49:27 -0500

    Add ideas for articles - as a placeholder.

--- index.php
+++ index.php
@@ -62,17 +62,19 @@ $urlPath =
 			</section>
 			<section>
 				<article>
-					<h2>Managing Chaos</h2>
-					<p>Using JIRA to manage projects...</p>
+					<h2>PHP MVC Pattern</h2>
+					<p>Content...</p>
 				</article>
-
-<!--
-				<p>That's a very limited life. Life can be much broader, once you discover one simple fact, and that is that everything around you that you call life was made up by people that were no smarter than you. And you can change it, you can influence it, you can build your own things that other people can use. Once you learn that, you'll never be the same again. - Steve Jobs (http://www.youtube.com/watch?feature=player_embedded&v=kYfNvmF0Bqw)</p>
--->
+				<article>
+					<h2>Jenkins & NAnt</h2>
+					<p>Content...</p>
+				</article>
 			</section>
+<!--
 			<section>
 				<p>That's a very limited life. Life can be much broader, once you discover one simple fact, and that is that everything around you that you call life was made up by people that were no smarter than you. And you can change it, you can influence it, you can build your own things that other people can use. Once you learn that, you'll never be the same again. - Steve Jobs (http://www.youtube.com/watch?feature=player_embedded&v=kYfNvmF0Bqw)</p>
 			</section>
+ -->
 		</div>
 		<footer class="columns two">
 			<section>
