5d9c247292e135c2cb7bfda899eb0026f301b50e
Matt Choinski <matt@choinski.net>
2014-02-06 15:56:54 -0500

    Add Google Analytics to the website.

--- index.php
+++ index.php
@@ -80,5 +80,14 @@ $urlPath =
 		</footer>
 		<script src="<?php echo( $urlPath ); ?>/include/script/jquery.min.js"></script>
 		<script src="<?php echo( $urlPath ); ?>/include/script/script.min.js" type="text/javascript"></script>
+		<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-12259620-1', 'choinski.net');
+ga('send', 'pageview');
+		</script>
 	</body>
 </html>
