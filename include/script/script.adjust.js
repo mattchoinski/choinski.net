71ef89c620332af8ef0aa83cebd38676da2d7a68
Matthew Choinski <hello@choinski.net>
2016-05-23 14:05:25 -0400

    Added scripts for expanding the body to place the footer at the bottom of a view port.

---
+++ include/script/script.adjust.js
@@ -0,0 +1,21 @@
+﻿docReady(
+    function() {
+        var elementBody = document.body;
+        var elementBodyStyle = elementBody.style;
+        var elementFooter = elementBody.getElementsByTagName("footer")[0];
+        var elementFooterStyle = elementFooter.style;
+        var elementHtmlStyle = document.documentElement.style;
+        elementBodyStyle.height = "auto";
+        elementHtmlStyle.height = "auto";
+        if (elementFooter.getAttribute("bottom") !== undefined) {
+            elementFooter.removeAttribute("bottom")
+        }
+        elementFooterStyle.position = "relative";
+        if (window.innerHeight > elementBody.clientHeight) {
+            elementBodyStyle.height = "100%";
+            elementHtmlStyle.height = "100%";
+            elementFooterStyle.bottom = "0";
+            elementFooterStyle.position = "absolute";
+        }
+    }
+);
